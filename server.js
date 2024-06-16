fcbe0c031370f3f029ee8926fbc7c88b55cec5f6
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-06-16 21:45:16 +0100

    finallly

--- server.js
+++ server.js
@@ -11,7 +11,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+
+
+app.use(cors({
+  origin: ["https://datafetchfrontend.onrender.com","http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://kiksymyguy:Michealanike123@cluster0.78vjdgj.mongodb.net/my_database')
